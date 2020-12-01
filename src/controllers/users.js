348d946ca5ed05d067ae8eec39f3311832a8fbae
Muhammad Ilyas <53975283+milyas26@users.noreply.github.com>
2020-12-02 00:13:32 +0700

    add Register API

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,3 +1,17 @@
+exports.register = (req, res) => {
+  const { email, password, channelName, description } = req.body
+
+  res.send({
+    status: 'Success',
+    data: {
+      channel: {
+        email: email,
+        password: password,
+      },
+    },
+  })
+}
+
 exports.login = (req, res) => {
   const { email, password } = req.body
 
