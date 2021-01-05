98111217c523b5334164d7849a03d39b7c14156f
Muhammad Ilyas <muhammadilyassaya@gmail.com>
2021-01-05 14:55:30 +0700

    edit models

--- models/comment.js
+++ models/comment.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       Comment.belongsTo(models.Video, {
         as: "video",
+        foreignKey: "videoId",
       });
       Comment.belongsTo(models.Channel, {
         as: "channel",
